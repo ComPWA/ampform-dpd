ca7a12bf19d06180d5cf390518de76dd27a2e360
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-10-05 16:19:01 +0200

    DOC!: add basic documentation with Sphinx

---
+++ docs/conf.py
@@ -0,0 +1,147 @@
+import os
+import shutil
+import subprocess
+import sys
+from datetime import datetime
+
+if sys.version_info < (3, 8):
+    from importlib_metadata import PackageNotFoundError
+    from importlib_metadata import version as get_package_version
+else:
+    from importlib.metadata import PackageNotFoundError
+    from importlib.metadata import version as get_package_version
+
+sys.path.insert(0, os.path.abspath("."))
+from _relink_references import relink_references
+
+
+def get_execution_mode() -> str:
+    if "FORCE_EXECUTE_NB" in os.environ:
+        print("\033[93;1mWill run ALL Jupyter notebooks!\033[0m")
+        return "force"
+    if "EXECUTE_NB" in os.environ:
+        print("\033[93;1mWill run Jupyter notebooks with cache\033[0m")
+        return "cache"
+    return "off"
+
+
+def get_timestamp() -> str:
+    now = datetime.now()
+    return now.strftime("%d/%m/%Y %H:%M:%S")
+
+
+def get_version() -> str:
+    try:
+        return get_package_version("ampform_dpd")
+    except PackageNotFoundError:
+        return ""
+
+
+def generate_api() -> None:
+    shutil.rmtree("api", ignore_errors=True)
+    subprocess.call(
+        " ".join(
+            [
+                "sphinx-apidoc",
+                f"../src/ampform_dpd/",
+                f"../src/ampform_dpd/version.py",
+                "-o api/",
+                "--force",
+                "--no-toc",
+                "--separate",
+                "--templatedir _templates",
+            ]
+        ),
+        shell=True,
+    )
+
+
+generate_api()
+relink_references()
+
+
+add_module_names = False
+author = "Mikhail Mikhasenko, Remco de Boer"
+autodoc_default_options = {
+    "exclude-members": ", ".join(
+        [
+            "default_assumptions",
+            "doit",
+            "evaluate",
+            "is_commutative",
+            "is_extended_real",
+        ]
+    ),
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
+autodoc_member_order = "bysource"
+autodoc_type_aliases = {}
+autodoc_typehints_format = "short"
+autosectionlabel_prefix_document = True
+autosectionlabel_maxdepth = 2
+codeautolink_concat_default = True
+copyright = "2022"
+default_role = "py:obj"
+exclude_patterns = [
+    "**.ipynb_checkpoints",
+    ".DS_Store",
+    "Thumbs.db",
+    "_build",
+]
+extensions = [
+    "myst_nb",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_book_theme",
+    "sphinx_codeautolink",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinx_togglebutton",
+]
+html_sourcelink_suffix = ""
+html_theme = "sphinx_book_theme"
+html_theme_options = {
+    "extra_navbar": f"<p>Version {get_version()} ({get_timestamp()})</p>",
+    "launch_buttons": {
+        "binderhub_url": "https://mybinder.org",
+    },
+    "path_to_docs": "docs",
+    "repository_url": "https://github.com/ComPWA/ampform_dpd",
+    "repository_branch": "main",
+    "show_navbar_depth": 2,
+    "show_toc_level": 2,
+    "use_repository_button": True,
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+}
+html_title = "Symbolic Dalitz-Plot Decomposition"
+intersphinx_mapping = {
+    "IPython": ("https://ipython.readthedocs.io/en/stable", None),
+    "ampform": (f"https://ampform.readthedocs.io/en/stable", None),
+    "attrs": ("https://www.attrs.org/en/stable", None),
+    "python": ("https://docs.python.org/3", None),
+    "sympy": ("https://docs.sympy.org/latest", None),
+}
+myst_enable_extensions = [
+    "colon_fence",
+    "dollarmath",
+    "html_image",
+    "substitution",
+]
+myst_render_markdown_format = "myst"
+nb_execution_allow_errors = False
+nb_execution_mode = get_execution_mode()
+nb_execution_show_tb = True
+nb_execution_timeout = -1
+nb_output_stderr = "show"
+nb_render_markdown_format = "myst"
+nitpicky = True
+numfig = True
+primary_domain = "py"
+pygments_style = "sphinx"
+version = get_version()
+viewcode_follow_imported_members = True
