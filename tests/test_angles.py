17123fd33e6a1e863aaf81f785d73b2f483b632c
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-01-29 02:09:35 +0100

    MAINT: revert `docformatter` changes

--- tests/test_angles.py
+++ tests/test_angles.py
@@ -53,9 +53,7 @@ def test_formulate_theta_hat_angle():
 
 
 def test_formulate_zeta_angle_equation_A6():
-    """Test Eq.
-
-    (A6), https://journals.aps.org/prd/pdf/10.1103/PhysRevD.101.034033#page=10.
+    """Test Eq. (A6), https://journals.aps.org/prd/pdf/10.1103/PhysRevD.101.034033#page=10.
     """
     for i in [1, 2, 3]:
         for k in [1, 2, 3]:
