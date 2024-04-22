d872b92414914e806b3099d2551df12bb00113d2
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-04-22 15:05:42 +0200

    FEAT: implement `to_three_body_decay()`

--- src/ampform_dpd/adapter/qrules.py
+++ src/ampform_dpd/adapter/qrules.py
@@ -7,7 +7,59 @@ from qrules.quantum_numbers import InteractionProperties
 from qrules.topology import EdgeType, FrozenTransition, NodeType
 from qrules.transition import State
 
-from ampform_dpd.decay import LSCoupling, Particle
+from ampform_dpd.decay import (
+    IsobarNode,
+    LSCoupling,
+    Particle,
+    ThreeBodyDecay,
+    ThreeBodyDecayChain,
+)
+
+
+def to_three_body_decay(
+    transitions: Iterable[FrozenTransition[Particle, LSCoupling]],
+) -> ThreeBodyDecay:
+    transitions = tuple(transitions)
+    if not transitions:
+        msg = "Need at least one transition object"
+        raise ValueError(msg)
+    some_transition = transitions[0]
+    return ThreeBodyDecay(
+        states={**some_transition.initial_states, **some_transition.final_states},
+        chains=tuple(sorted(to_decay_chain(t) for t in transitions)),
+    )
+
+
+def to_decay_chain(
+    transition: FrozenTransition[Particle, LSCoupling],
+) -> ThreeBodyDecayChain:
+    if len(transition.initial_states) != 1:
+        msg = f"Can only handle one initial state, but got {len(transition.initial_states)}"
+        raise ValueError(msg)
+    if len(transition.final_states) != 3:  # noqa: PLR2004
+        msg = f"Can only handle three final states, but got {len(transition.final_states)}"
+        raise ValueError(msg)
+    if len(transition.interactions) != 2:  # noqa: PLR2004
+        msg = f"There are {len(transition.interactions)} interaction nodes, so this can't be a three-body decay"
+        raise ValueError(msg)
+    topology = transition.topology
+    spectator_id, resonance_id = sorted(topology.get_edge_ids_outgoing_from_node(0))
+    resonance_id, *_ = sorted(topology.get_edge_ids_ingoing_to_node(1))
+    child1_id, child2_id = sorted(topology.get_edge_ids_outgoing_from_node(1))
+    parent, *_ = transition.initial_states.values()
+    production_node, decay_node = transition.interactions.values()
+    isobar = IsobarNode(
+        parent=parent,
+        child1=IsobarNode(
+            parent=transition.states[resonance_id],
+            child1=transition.states[child1_id],
+            child2=transition.states[child2_id],
+            interaction=decay_node,
+        ),
+        child2=transition.states[spectator_id],
+        interaction=production_node,
+    )
+    return ThreeBodyDecayChain(decay=isobar)
 
 
 def convert_edges_and_nodes(
