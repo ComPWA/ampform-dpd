02875395e2b9e1bb00994293db9ac1628827b749
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2025-03-07 16:10:51 +0100

    FEAT: implement `use_coefficients` switch (#159)
    * DOC: show `use_coefficients` example in documentation
    * DX: check number of couplings products
    * DX: test if n_decay * n_prod == n_combinations
    * DX: implement test for `use_coefficients`
    * MAINT: update hashes in unit tests
    * MAINT: update Jupyter kernels

--- tests/test_dpd_builder.py
+++ tests/test_dpd_builder.py
@@ -4,8 +4,10 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import pytest
+import qrules
+import sympy as sp
 
-from ampform_dpd import DalitzPlotDecompositionBuilder
+from ampform_dpd import AmplitudeModel, DalitzPlotDecompositionBuilder
 from ampform_dpd.adapter.qrules import normalize_state_ids, to_three_body_decay
 from ampform_dpd.dynamics.builder import formulate_breit_wigner_with_form_factor
 
@@ -53,3 +55,148 @@ class TestDalitzPlotDecompositionBuilder:
         if not all_subsystems:
             expected_variables.remove("theta_23")
         assert {s.name for s in model.variables} == expected_variables
+
+    @pytest.mark.parametrize("min_ls", [False, True])
+    @pytest.mark.parametrize("use_coefficients", [False, True])
+    def test_use_coefficients(
+        self, jpsi2pksigma_reaction: ReactionInfo, min_ls: bool, use_coefficients: bool
+    ):
+        if jpsi2pksigma_reaction.formalism == "helicity" and not min_ls:
+            pytest.skip("Helicity formalism with all LS not supported")
+        transitions = normalize_state_ids(jpsi2pksigma_reaction.transitions)
+        decay = to_three_body_decay(transitions, min_ls=min_ls)
+        builder = DalitzPlotDecompositionBuilder(decay, min_ls=min_ls)
+        model = builder.formulate(
+            reference_subsystem=2,
+            use_coefficients=use_coefficients,
+        )
+        amplitudes = _get_physical_amplitudes(model)
+        coupling_symbols = _collect_indexed_symbols(amplitudes)
+
+        n_coupling_symbols = len(coupling_symbols)
+        coupling_symbols_str = sorted(str(s) for s in coupling_symbols)
+        # ----==== COEFFICIENTS ===--- #
+        if use_coefficients:
+            if min_ls:  # HELICITY BASIS
+                assert n_coupling_symbols == 20
+                assert coupling_symbols_str == [
+                    R"\mathcal{H}^\mathrm{N(1700)^{+}}[-1/2, -1/2, 0, -1/2]",
+                    R"\mathcal{H}^\mathrm{N(1700)^{+}}[-1/2, -1/2, 0, 1/2]",
+                    R"\mathcal{H}^\mathrm{N(1700)^{+}}[-1/2, 1/2, 0, -1/2]",
+                    R"\mathcal{H}^\mathrm{N(1700)^{+}}[-1/2, 1/2, 0, 1/2]",
+                    R"\mathcal{H}^\mathrm{N(1700)^{+}}[-3/2, -1/2, 0, -1/2]",
+                    R"\mathcal{H}^\mathrm{N(1700)^{+}}[-3/2, -1/2, 0, 1/2]",
+                    R"\mathcal{H}^\mathrm{N(1700)^{+}}[1/2, -1/2, 0, -1/2]",
+                    R"\mathcal{H}^\mathrm{N(1700)^{+}}[1/2, -1/2, 0, 1/2]",
+                    R"\mathcal{H}^\mathrm{N(1700)^{+}}[1/2, 1/2, 0, -1/2]",
+                    R"\mathcal{H}^\mathrm{N(1700)^{+}}[1/2, 1/2, 0, 1/2]",
+                    R"\mathcal{H}^\mathrm{N(1700)^{+}}[3/2, 1/2, 0, -1/2]",
+                    R"\mathcal{H}^\mathrm{N(1700)^{+}}[3/2, 1/2, 0, 1/2]",
+                    R"\mathcal{H}^\mathrm{\overline{\Sigma}(1660)^{-}}[-1/2, -1/2, -1/2, 0]",
+                    R"\mathcal{H}^\mathrm{\overline{\Sigma}(1660)^{-}}[-1/2, -1/2, 1/2, 0]",
+                    R"\mathcal{H}^\mathrm{\overline{\Sigma}(1660)^{-}}[-1/2, 1/2, -1/2, 0]",
+                    R"\mathcal{H}^\mathrm{\overline{\Sigma}(1660)^{-}}[-1/2, 1/2, 1/2, 0]",
+                    R"\mathcal{H}^\mathrm{\overline{\Sigma}(1660)^{-}}[1/2, -1/2, -1/2, 0]",
+                    R"\mathcal{H}^\mathrm{\overline{\Sigma}(1660)^{-}}[1/2, -1/2, 1/2, 0]",
+                    R"\mathcal{H}^\mathrm{\overline{\Sigma}(1660)^{-}}[1/2, 1/2, -1/2, 0]",
+                    R"\mathcal{H}^\mathrm{\overline{\Sigma}(1660)^{-}}[1/2, 1/2, 1/2, 0]",
+                ]
+            else:  # CANONICAL BASIS
+                assert n_coupling_symbols == 4
+                assert coupling_symbols_str == [
+                    R"\mathcal{H}^\mathrm{LS,N(1700)^{+}}[1, 1, 2, 1/2]",
+                    R"\mathcal{H}^\mathrm{LS,N(1700)^{+}}[1, 2, 2, 1/2]",
+                    R"\mathcal{H}^\mathrm{LS,\overline{\Sigma}(1660)^{-}}[0, 1, 1, 1/2]",
+                    R"\mathcal{H}^\mathrm{LS,\overline{\Sigma}(1660)^{-}}[2, 1, 1, 1/2]",
+                ]
+        # ----==== COUPLING ===--- #
+        else:
+            n_products = len(_collect_products(amplitudes))
+            if min_ls:  # HELICITY BASIS
+                assert n_coupling_symbols == 14
+                assert n_products == 20
+                assert coupling_symbols_str == [
+                    R"\mathcal{H}^\mathrm{decay}[N(1700)^{+}, 0, -1/2]",
+                    R"\mathcal{H}^\mathrm{decay}[N(1700)^{+}, 0, 1/2]",
+                    R"\mathcal{H}^\mathrm{decay}[\overline{\Sigma}(1660)^{-}, -1/2, 0]",
+                    R"\mathcal{H}^\mathrm{decay}[\overline{\Sigma}(1660)^{-}, 1/2, 0]",
+                    R"\mathcal{H}^\mathrm{production}[N(1700)^{+}, -1/2, -1/2]",
+                    R"\mathcal{H}^\mathrm{production}[N(1700)^{+}, -1/2, 1/2]",
+                    R"\mathcal{H}^\mathrm{production}[N(1700)^{+}, -3/2, -1/2]",
+                    R"\mathcal{H}^\mathrm{production}[N(1700)^{+}, 1/2, -1/2]",
+                    R"\mathcal{H}^\mathrm{production}[N(1700)^{+}, 1/2, 1/2]",
+                    R"\mathcal{H}^\mathrm{production}[N(1700)^{+}, 3/2, 1/2]",
+                    R"\mathcal{H}^\mathrm{production}[\overline{\Sigma}(1660)^{-}, -1/2, -1/2]",
+                    R"\mathcal{H}^\mathrm{production}[\overline{\Sigma}(1660)^{-}, -1/2, 1/2]",
+                    R"\mathcal{H}^\mathrm{production}[\overline{\Sigma}(1660)^{-}, 1/2, -1/2]",
+                    R"\mathcal{H}^\mathrm{production}[\overline{\Sigma}(1660)^{-}, 1/2, 1/2]",
+                ]
+            else:  # CANONICAL BASIS
+                assert n_coupling_symbols == 6
+                assert n_products == 4
+                assert coupling_symbols_str == [
+                    R"\mathcal{H}^\mathrm{LS,decay}[N(1700)^{+}, 2, 1/2]",
+                    R"\mathcal{H}^\mathrm{LS,decay}[\overline{\Sigma}(1660)^{-}, 1, 1/2]",
+                    R"\mathcal{H}^\mathrm{LS,production}[N(1700)^{+}, 1, 1]",
+                    R"\mathcal{H}^\mathrm{LS,production}[N(1700)^{+}, 1, 2]",
+                    R"\mathcal{H}^\mathrm{LS,production}[\overline{\Sigma}(1660)^{-}, 0, 1]",
+                    R"\mathcal{H}^\mathrm{LS,production}[\overline{\Sigma}(1660)^{-}, 2, 1]",
+                ]
+
+    @pytest.mark.parametrize("basis", ["canonical", "helicity"])
+    @pytest.mark.parametrize("resonance", ["N(1675)", "Sigma(1775)"])
+    def test_use_coefficients_combinations(self, basis: str, resonance: str):  # noqa: PLR0914
+        reaction = qrules.generate_transitions(
+            initial_state=[("J/psi(1S)", [+1])],
+            final_state=[("Sigma+", [+0.5]), "K0", ("p~", [+0.5])],
+            allowed_interaction_types="strong",
+            allowed_intermediate_particles=[resonance],
+            formalism="canonical-helicity",
+        )
+        transitions = normalize_state_ids(reaction.transitions)
+        min_ls = basis == "helicity"
+        decay = to_three_body_decay(transitions, min_ls)
+        builder = DalitzPlotDecompositionBuilder(decay, min_ls)
+        # cspell:ignore coeff
+        reference_subsystem = 1 if resonance.startswith("Sigma") else 3
+        coupling_model = builder.formulate(reference_subsystem)  # type:ignore[arg-type]
+        coeff_model = builder.formulate(reference_subsystem, use_coefficients=True)  # type:ignore[arg-type]
+        coupling_amplitudes = _get_physical_amplitudes(coupling_model)
+        coeff_amplitudes = _get_physical_amplitudes(coeff_model)
+
+        couplings = _collect_indexed_symbols(coupling_amplitudes)
+        coefficients = _collect_indexed_symbols(coeff_amplitudes)
+        coupling_products = _collect_products(coupling_amplitudes)
+
+        n_couplings = len(couplings)
+        n_decay_couplings = len({s for s in couplings if "decay" in s.name})
+        n_production_couplings = len({s for s in couplings if "production" in s.name})
+        assert n_couplings == n_decay_couplings + n_production_couplings
+
+        n_coupling_products = len(coupling_products)
+        n_coefficients = len(coefficients)
+        assert n_coefficients == n_coupling_products
+        assert n_coefficients == n_decay_couplings * n_production_couplings
+
+
+def _collect_indexed_symbols(amplitudes: list[sp.Expr]) -> set[sp.Indexed]:
+    coupling_symbols: set[str] = set()
+    for expr in amplitudes:
+        symbols = {s for s in expr.free_symbols if isinstance(s, sp.Indexed)}
+        coupling_symbols.update(symbols)
+    return coupling_symbols
+
+
+def _collect_products(amplitudes: list[sp.Expr]) -> list[tuple[sp.Indexed, sp.Indexed]]:
+    products = set()
+    for amp in amplitudes:
+        for node in sp.postorder_traversal(amp):
+            couplings = {s for s in node.free_symbols if isinstance(s, sp.Indexed)}
+            if len(couplings) == 2:
+                products.add(tuple(sorted(couplings, key=str)))
+    return sorted(products, key=str)
+
+
+def _get_physical_amplitudes(model: AmplitudeModel) -> list[sp.Expr]:
+    amplitudes = [expr.doit() for expr in model.amplitudes.values()]
+    return [expr for expr in amplitudes if expr]
