a0ce4f117165c7f0227de80cbb37f239a96320e6
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2026-08-06 15:23:58 +0200

    FEAT: add serialization workspace loader

--- src/ampform_dpd/io/serialization/__init__.py
+++ src/ampform_dpd/io/serialization/__init__.py
@@ -7,3 +7,7 @@ https://rub-ep1.github.io/amplitude-serialization for more information.
     This module is in preview, see https://github.com/ComPWA/ampform-dpd/issues/133 for
     updates.
 """
+
+from ampform_dpd.io.serialization.workspace import Workspace, load_workspace
+
+__all__ = ["Workspace", "load_workspace"]
